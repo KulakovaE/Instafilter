0c1f1c21f90516bceae2f74109fe371cce624dd2
Elena Kulakova <egeorgievska89@gmail.com>
2020-02-13 10:18:43 +0100

    Initial Commit

---
+++ Instafilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Elena Kulakova on 2020-02-13.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
