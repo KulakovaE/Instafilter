944dc9cdb0a27843b07cb6ec4941fb58928860a6
Elena Kulakova <egeorgievska89@gmail.com>
2020-02-13 10:45:41 +0100

    Designing the interface with Stroyboard.

---
+++ Instafilter/ImageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Elena Kulakova on 2020-02-13.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func changeFilterTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+        
+    }
+    
+
+}
+
